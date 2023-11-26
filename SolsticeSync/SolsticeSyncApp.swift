e32e5c22c2e710a282a3650b26e52cccd7e7afb1
Eldrago12 <E22CSEU0776@outlook.com>
2023-11-26 16:43:21 +0530

    Initial Commit

---
+++ SolsticeSync/SolsticeSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolsticeSyncApp.swift
+//  SolsticeSync
+//
+//  Created by SIRSHAK DOLAI on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SolsticeSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
